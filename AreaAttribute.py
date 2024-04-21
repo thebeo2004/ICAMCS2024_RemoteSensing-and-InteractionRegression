1b37f8309b7f305858496ea81b13d3b856af79d8
theobeo2004 <thenhuet2004@gmail.com>
2024-04-21 15:46:11 +0700

    data: fixing misposition between data & file names

--- AreaAttribute.py
+++ AreaAttribute.py
@@ -1,10 +1,12 @@
 import arcpy 
 
-folders = ["C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Ha_Noi", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Hai_Duong", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Hai_Phong", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Hung_Yen", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Nam_Dinh", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Ninh_Binh", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Quang_Ninh", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Thai_Binh", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Vinh_Phuc", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Bac_Ninh", "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)\Ha_Nam"]
+folder = "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)"
 
-for province in folders:
+provinces = ['Ha_Noi', 'Hai_Duong', 'Hai_Phong', 'Hung_Yen', 'Nam_Dinh', 'Ninh_Binh', 'Quang_Ninh', 'Thai_Binh', 'Vinh_Phuc', 'Bac_Ninh', 'Ha_Nam']
+
+for province in provinces:
     for year in range (2000, 2021):
-        file = province + "\\" + str(year) + ".shp"
+        file = folder + "\\" + province + "\\" + str(year) + ".shp"
         arcpy.AddField_management(file, "area", "DOUBLE")
         with arcpy.da.UpdateCursor(file, ["SHAPE@AREA", "area"]) as cursor:
             for row in cursor:
--- RasterToPolygon(ArcPy API).py
+++ RasterToPolygon(ArcPy API).py
@@ -1,14 +1,14 @@
-import arcpy
-inputs = ["C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Ha Noi", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Hai Duong", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Hai Phong", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Hung Yen", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Nam Dinh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Ninh Binh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Quang Ninh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Thai Binh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover (2000 - 2020)\Vinh Phuc"]
-outputs = ["C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Ha Noi", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Hai Duong", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Hai Phong", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Hung Yen", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Nam Dinh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Ninh Binh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Quang Ninh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Thai Binh", "C:\Users\CA_UOP_MUOI\OneDrive - vnu.edu.vn\Dataset (Crop Yield Prediction)\Practice\Province LandCover Shape (2000 - 2020)\Vinh Phuc"]
-length = len(inputs)
+import arcpy   
+input_folder = "C:\Application\Crop-Yield-Prediction\Data\Province LandCover (2000 - 2020)"
+output_folder = "C:\Application\Crop-Yield-Prediction\Data\Province LandCover Shape (2000 - 2020)"
 
-for i in range(length):
-    arcpy.env.workspace = inputs[i]
+provinces = ['Ha_Noi', 'Hai_Duong', 'Hai_Phong', 'Hung_Yen', 'Nam_Dinh', 'Ninh_Binh', 'Quang_Ninh', 'Thai_Binh', 'Vinh_Phuc', 'Bac_Ninh', 'Ha_Nam']
+
+for province in provinces:
+    arcpy.env.workspace = input_folder + "\\" + province 
     dataset = arcpy.ListRasters()
-    folder = outputs[i] + "\\"
     year = 2000
     for raster in dataset:
-        raster_out = folder + str(year)
+        output_file = output_folder + "\\" + province + "\\" + str(year)
         year += 1
-        arcpy.conversion.RasterToPolygon(raster, raster_out, "NO_SIMPLIFY", "VALUE")
+        arcpy.conversion.RasterToPolygon(raster, output_file, "NO_SIMPLIFY", "VALUE")
