9c0ac380eea8bce7b08038f7e3bd7ce11ee22f64
Cá Ướp Muối <72870812+thebeo2004@users.noreply.github.com>
2024-06-28 23:42:48 +0000

    feat: minor changes, replace absolute path by relative one

--- Prediction/FarmingSeason/SplittingData.py
+++ Prediction/FarmingSeason/SplittingData.py
@@ -1,9 +1,14 @@
 import pandas as pd
+from pathlib import Path
 
 def splitting_data():
+    
+    dir_path = Path(__file__).parent.parent.parent.absolute()
+    weather_path = dir_path / 'Data/Farming Season/Weather(Farming Season).csv'
+    yield_path = dir_path / 'Data/Farming Season/RandomCropYield (Farming Season).csv'
 
-    weather_df = pd.read_csv('C:\Application\Crop-Yield-Prediction\Data\Farming Season\Weather(Farming Season).csv')
-    yield_df = pd.read_csv('C:\Application\Crop-Yield-Prediction\Data\Farming Season\RandomCropYield (Farming Season).csv')
+    weather_df = pd.read_csv(weather_path)
+    yield_df = pd.read_csv(yield_path)
 
     X_train = weather_df.loc[weather_df['Year'] < 2017].drop(columns=['Year', 'Province'])
     y_train = yield_df.loc[yield_df['Year'] < 2017].drop(columns=['Year', 'Province'])
