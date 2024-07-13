fae84f98222f7f320e044c02164208bbdaaa4da5
theobeo2004 <thenhuet2004@gmail.com>
2024-07-13 21:26:04 +0700

    feat: apply time-wise k-fold on dataset

---
+++ Prediction/TimewiseKfold.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+from pathlib import Path
+from sklearn.model_selection import TimeSeriesSplit
+
+def splitting_data():
+    
+    dir_path = Path(__file__).parent.parent.absolute()
+    weather_path = dir_path / 'Data/Weather.csv'
+    yield_path = dir_path / 'Data/RandomCropYield.csv'
+    
+    weather_df = pd.read_csv(weather_path)
+    yield_df = pd.read_csv(yield_path)
+    
+    X = weather_df.drop(columns=['Year', 'Province'])
+    y = yield_df.drop(columns=['Year', 'Province'])
+    
+    folds = []
+    
+    tscv = TimeSeriesSplit(n_splits=3, test_size=7)
+    
+    for (train_index, test_index) in (tscv.split(y)):
+        folds.append({
+            'X_train': X.iloc[train_index],
+            'y_train': y.iloc[train_index],
+            'X_test': X.iloc[test_index],
+            'y_test': y.iloc[test_index]
+        })
+    
+    return folds
