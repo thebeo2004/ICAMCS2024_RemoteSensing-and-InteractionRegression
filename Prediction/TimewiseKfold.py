e78840778d06e5dc51ea4d4269c04481b8afbacc
theobeo2004 <thenhuet2004@gmail.com>
2024-07-20 15:25:06 +0700

    feat: feature scaling by min-max

--- Prediction/TimewiseKfold.py
+++ Prediction/TimewiseKfold.py
@@ -1,10 +1,11 @@
 import pandas as pd 
 from pathlib import Path
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import MinMaxScaler
 
 FOLD_NUM = 3
 
-def splitting_data():
+def splitting_data(is_scaled=False):
     
     dir_path = Path(__file__).parent.parent.absolute()
     weather_path = dir_path / 'Data/Weather.csv'
@@ -15,6 +16,10 @@ def splitting_data():
     
     X = weather_df.drop(columns=['Year', 'Province'])
     y = yield_df.drop(columns=['Year', 'Province'])
+    
+    if (is_scaled):
+        X_scaled = MinMaxScaler().fit_transform(X)
+        X = pd.DataFrame(X_scaled, columns=X.columns)
     
     folds = []
     
@@ -32,4 +37,3 @@ def splitting_data():
 
 def split_train_test(fold):
     return fold['X_train'], fold['X_test'], fold['y_train'], fold['y_test']
-
