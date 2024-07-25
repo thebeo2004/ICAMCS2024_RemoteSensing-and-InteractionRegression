582b83c49a9bb7c4e096697d21caf1895efe52ba
theobeo2004 <thenhuet2004@gmail.com>
2024-07-25 20:54:11 +0700

    feat: significantly reduce computation time by optimizing number of combinations

--- Prediction/InteractionDetection.py
+++ Prediction/InteractionDetection.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import root_mean_squared_error
-from TimewiseKfold import split_train_test, splitting_data
+from TimewiseKfold import split_train_test, split_train_validation
 
 def initialize(X):
     
@@ -22,19 +22,6 @@ def Step0(attribute_num, K):
     deltas_res = np.zeros((1, K))
     
     return alphas_res, deltas_res, M
-
-def Step1(attribute_num, alphas_res, M):
-    
-    alphas_pred = np.zeros(shape=(M, attribute_num))
-    
-    for i in range(0, M - 1):
-        for j in range(0, attribute_num):
-            alphas_pred[i][j] = alphas_res[i][j]
-            
-    for j in range(0, attribute_num):
-        alphas_pred[M - 1][j] = 0.5
-        
-    return alphas_pred
 
 def check_neighborhood(alpha):
     if (np.abs(alpha - 0.5).sum() <= 1):
@@ -70,37 +57,29 @@ def d_hop_neighborood(alpha_set, alpha, d, start):
         d_hop_neighborood(alpha_set, new_alpha1, d - 1, i + 1)
         d_hop_neighborood(alpha_set, new_alpha2, d - 1, i + 1)
 
-def neighbors_generation(cur_neighbor, M, id, alpha_sets, neighbor_set):
+def alpha_generation(attribute_num):
+    alpha_set = []
     
-    if (id == M):
-        neighbor_set.append(np.array(cur_neighbor))
-        return
+    alpha = np.zeros(attribute_num) + 0.5
+    
+    for d in range(0, 3):
+        d_hop_neighborood(alpha_set=alpha_set, alpha=alpha, d=d, start=0)
+    
+    return alpha_set
 
-    for alpha in alpha_sets[id]:
-        
-        new_neighbor = initialize(cur_neighbor)
-        new_neighbor.append(alpha)
-        neighbors_generation(cur_neighbor=new_neighbor, M=M, id=(id + 1), alpha_sets=alpha_sets, neighbor_set=neighbor_set)
+def delta_generation(K):
 
-def Step2(alphas_pred, M, attribute_num):
-    
-    alpha_sets = []
-    
-    for alpha in alphas_pred:
+    delta_set = []
+
+    for i in range(0, K):
+
+        delta = np.zeros(K)
         
-        new_alpha = np.array(alpha)
-        alpha_set = []
-        
-        for d in range(0, 3):
-            d_hop_neighborood(alpha_set=alpha_set, alpha=new_alpha, d=d, start=0)
-            
-        alpha_sets.append(alpha_set)
+        delta[i] = 1
+
+        delta_set.append(delta)
     
-    neighbor_set = []
-        
-    neighbors_generation(cur_neighbor=[], M=M, id=0, alpha_sets=alpha_sets, neighbor_set=neighbor_set)
-    
-    return neighbor_set  
+    return delta_set
 
 sigma_f = 0.5
 sigma_l = 0.5
@@ -129,126 +108,126 @@ def kernel_functions(r):
     
     return np.array([linear(r), exponential(1, r), exponential(2, r), matern(3, r), matern(5, r), quadratic(r)])
 
-def interaction_type(sample, alpha):
+def interaction_type_detection(alpha):
     
-    if (np.abs(alpha - 0.5).sum() > 1):
-        print('Exception in alpha when there are more than 2 elements that are not 0.5')
-        
-    index = []
+    interaction_type = []
     
     for i in range(0, len(alpha)):
         if (alpha[i] != 0.5):
-            index.append(i)
+            interaction_type.append(i)
+                
+    if (len(interaction_type) > 2):
+        print('There is an exeception that an alpha has more than 2 elements not equal to 0.5')
+    
+    return interaction_type
+
+def interaction_contribution(sample, interaction_type, alpha, delta):
     
     r = 0
 
-    if (len(index) == 2):
-        j = index[0]
-        l = index[1]
+    if (len(interaction_type) == 2):
+        j = interaction_type[0]
+        l = interaction_type[1]
         r = (2 * alpha[j] - 1) * (sample[j] + alpha[j] - 1) + (2 * alpha[l] - 1) * (-sample[l] - alpha[l] + 1)
     else:
-        j = index[0]
+        j = interaction_type[0]
         r = (2 * alpha[j] - 1) * (sample[j] + alpha[j] - 1)
     
-    return r
-        
+    return np.dot(delta, kernel_functions(r))
 
-def interactions(X, alphas, deltas):
-    
-    interactions_matrix = []
-    
-    for sample in X:
-        
-        row = []
-        
-        for (m, alpha) in enumerate(alphas):
-            
-            if (np.abs(alpha - 0.5).sum() == 0):
-                continue
-            
-            r = interaction_type(sample=sample, alpha=alpha)
-            
-            row.append(np.dot(deltas[m], kernel_functions(r)))
-
-        interactions_matrix.append(row)
-    
-    return interactions_matrix
-
-def combination(X, interactions_matrix):
+def adding_interaction(X, alphas=[], deltas=[], interaction_type=None):
     
     X_transformed = initialize(X)
     
-    for i, sample in enumerate(X_transformed):
-        if (len(interactions_matrix[i]) != 0):
-            sample += interactions_matrix[i]
-    
+    for i, alpha in enumerate(alphas):
+        
+        if (np.abs(alpha - 0.5).sum() == 0):
+            continue
+        
+        if (interaction_type is None):
+            interaction_type = interaction_type_detection(alpha=alpha)
+        
+        for sample in X_transformed:
+            sample.append(interaction_contribution(sample=sample, interaction_type=interaction_type, alpha=alpha, delta=deltas[i]))
+        
+        #Reset value
+        interaction_type = None
+         
     return X_transformed
-            
 
-def RMSE(X_train, y_train, X_validation, y_validation, alphas, deltas):
+def transform_data(scaled_folds, selected_features, alphas=[], deltas=[]):
     
-    X_train_transformed = X_train.to_numpy()
-    X_validation_transformed = X_validation.to_numpy()
+    transformed_folds = []
     
-    if (len(alphas) != 0):
-        X_train_transformed = combination(X=X_train_transformed, interactions_matrix=interactions(X=X_train_transformed, alphas=alphas, deltas=deltas))
-        X_validation_transformed = combination(X=X_validation_transformed, interactions_matrix=interactions(X=X_validation_transformed, alphas=alphas, deltas=deltas))
+    for fold in scaled_folds:
+        X_train, X_test, y_train, y_test = split_train_test(fold)
+        X_train_reduced = X_train[selected_features].to_numpy()
+        X_test_reduced = X_test[selected_features].to_numpy()
+        
+        X_train_transformed = adding_interaction(X=X_train_reduced, alphas=alphas, deltas=deltas)
+        X_test_transformed = adding_interaction(X=X_test_reduced, alphas=alphas, deltas=deltas)
+        
+        transformed_folds.append({
+            'X_train': X_train_transformed,
+            'y_train': y_train,
+            'X_test': X_test_transformed,
+            'y_test': y_test
+        })
     
-    linear_model = LinearRegression().fit(X_train_transformed, y_train)
+    return transformed_folds
+            
+def RMSE(X_train, y_train, X_validation, y_validation):
     
-    RMSE_train = root_mean_squared_error(y_true=y_train, y_pred=linear_model.predict(X_train_transformed))
-    RMSE_validation = root_mean_squared_error(y_true=y_validation, y_pred=linear_model.predict(X_validation_transformed))
+    linear_model = LinearRegression().fit(X_train, y_train)
+    
+    RMSE_train = root_mean_squared_error(y_true=y_train, y_pred=linear_model.predict(X_train))
+    RMSE_validation = root_mean_squared_error(y_true=y_validation, y_pred=linear_model.predict(X_validation))
     
     return RMSE_train, RMSE_validation
 
-def RMSE_FOLDS(scaled_folds, alphas, deltas, selected_features):
+def RMSE_FOLDS(transformed_folds, alphas=[], deltas=[], interaction_type=None):
     
     RMSE_train_avg = 0
     RMSE_validation_avg = 0
     
-    for fold in scaled_folds:
-        X_train, X_validation, y_train, y_validation = split_train_test(fold)
-        X_train_reduced = X_train[selected_features]
-        X_validation_reduced = X_validation[selected_features]
+    for fold in transformed_folds:
+        X_train, X_test, y_train, y_test = split_train_test(fold)
+        X_train, X_validation, y_train, y_validation = split_train_validation(X=X_train, y=y_train)
+        # X_train, X_validation, y_train, y_validation = split_train_test(fold)
         
-        RMSE_train_cur, RMSE_validation_cur = RMSE(X_train_reduced, y_train, X_validation_reduced, y_validation, alphas=alphas, deltas=deltas)
+        X_train_added = adding_interaction(X=X_train, alphas=alphas, deltas=deltas, interaction_type=interaction_type)
+        X_validation_added = adding_interaction(X=X_validation, alphas=alphas, deltas=deltas, interaction_type=interaction_type)
+        
+        RMSE_train_cur, RMSE_validation_cur = RMSE(X_train=X_train_added, y_train=y_train, X_validation=X_validation_added, y_validation=y_validation)
         
         RMSE_train_avg += RMSE_train_cur
         RMSE_validation_avg += RMSE_validation_cur 
         
-    return RMSE_train_avg/len(scaled_folds), RMSE_validation_avg/len(scaled_folds)
+    return RMSE_train_avg/len(transformed_folds), RMSE_validation_avg/len(transformed_folds)
         
-def Evaluate(scaled_folds, neighbor_set, deltas_set, selected_features):
+def Evaluate(transformed_folds, alpha_set, delta_set, interaction_types):
     
-    alphas_d = []
-    deltas_d = []
+    alpha_d = []
+    delta_d = []
     
+    RMSE_validation = 0
     RMSE_min = 999999999
     
-    for alphas in neighbor_set:
-        for deltas in deltas_set:
-            
-            RMSE_cur = RMSE_FOLDS(scaled_folds=scaled_folds, alphas=alphas, deltas=deltas, selected_features=selected_features)[0]
-            
-            if (RMSE_cur < RMSE_min):
-                RMSE_min = RMSE_cur
-                alphas_d = alphas
-                deltas_d = deltas 
+    # transformed_folds = transform_data(scaled_folds=scaled_folds, selected_features=selected_features, alphas=alphas_res, deltas=deltas_res)
     
-    return alphas_d, deltas_d 
+    for i, alpha in enumerate(alpha_set):
+        for delta in delta_set:
+            RMSE_train_cur, RMSE_validation_cur = RMSE_FOLDS(transformed_folds=transformed_folds, alphas=[alpha], deltas=[delta], interaction_type=interaction_types[i])
+            
+            if (RMSE_train_cur < RMSE_min):
+                RMSE_min = RMSE_train_cur
+                RMSE_validation = RMSE_validation_cur
+                alpha_d = alpha 
+                delta_d = delta
+                # print(alpha_d, delta_d, RMSE_train_cur, RMSE_validation_cur)
+    
+    return alpha_d, delta_d, RMSE_validation
 
-def deltas_generation(deltas_set, deltas_cur, id, M, K):
-    
-    if (id == M):
-        deltas_set.append(deltas_cur)
-        return 
-    
-    for j in range(0, K):
-        new_deltas = np.array(deltas_cur)
-        
-        new_deltas[id][j] = 1
-        
-        deltas_generation(deltas_set, new_deltas, id + 1, M, K)
         
 #folds: It has got 3 items, each item consists of 4 parts: X_train, X_test, y_train and y_test
 #selected_features: a set of common features among 3 set of selected features which are determined from each fold by Elastic Net
@@ -257,26 +236,42 @@ def heuristic_interaction_detection(scaled_folds, selected_features, K):
     
     attribute_num = len(selected_features)
     
-    alphas_res, deltas_res, M = Step0(attribute_num=attribute_num, K=K)
+    # if (M == -1):
+    alphas_res = []
+    deltas_res = []
+    M = 1
+    # else:
+    #     M += 1
 
+    alpha_set = alpha_generation(attribute_num=attribute_num)
+    
+    interaction_types = []
+    
+    for alpha in alpha_set:
+        interaction_types.append(interaction_type_detection(alpha=alpha))
+    
+    delta_set = delta_generation(K=K)
+    
     while(True):
-        alphas_pred = Step1(attribute_num=attribute_num, alphas_res=alphas_res, M=M)
+        
+        # print("Round:", M)
 
-        neighbor_set = Step2(alphas_pred=alphas_pred, M=M, attribute_num=attribute_num)
+        transformed_folds = transform_data(scaled_folds=scaled_folds, selected_features=selected_features, alphas=alphas_res, deltas=deltas_res)
+        
+        alpha_d, delta_d, RMSE_validatioin = Evaluate(transformed_folds=transformed_folds, alpha_set=alpha_set, delta_set=delta_set, interaction_types=interaction_types)
     
-        deltas_set = []
-        deltas_generation(deltas_set=deltas_set, deltas_cur=np.zeros((M, K)), id=0, M=M, K=K)
-    
-        alphas_d, deltas_d = Evaluate(scaled_folds=scaled_folds, neighbor_set=neighbor_set, deltas_set=deltas_set, selected_features=selected_features)
-    
-        if (RMSE_FOLDS(scaled_folds=scaled_folds, alphas=alphas_d, deltas=deltas_d, selected_features=selected_features)[1] < 
-        RMSE_FOLDS(scaled_folds=scaled_folds, alphas=alphas_res, deltas=deltas_res, selected_features=selected_features)[1]):
-            alphas_res = alphas_d
-            deltas_res = deltas_d
+        if (RMSE_FOLDS(transformed_folds=transformed_folds)[1] > RMSE_validatioin):
+            alphas_res.append(alpha_d)
+            deltas_res.append(delta_d)
             M = M + 1
+            # if (M == 9):
+            #     break
+            # break
         else:
             break
     
-    rmse_train, rmse_validation = RMSE_FOLDS(scaled_folds=scaled_folds, alphas=alphas_res, deltas=deltas_res, selected_features=selected_features)
+    
+    transformed_folds = transform_data(scaled_folds=scaled_folds, selected_features=selected_features, alphas=alphas_res, deltas=deltas_res)
+    rmse_train, rmse_validation = RMSE_FOLDS(transformed_folds=transformed_folds, alphas=alphas_res, deltas=deltas_res)
     
     return alphas_res, deltas_res, rmse_train, rmse_validation
